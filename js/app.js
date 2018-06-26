7ef03c9ca5abe8cf73b057e89785edeb3b3d6740
Daniela Küster <daniela.kuester@gmx.net>
2018-06-26 15:54:55 +0200

    Add: Make enemies reappear at left side

--- js/app.js
+++ js/app.js
@@ -17,6 +17,9 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+		if (this.x >= 505) {
+				this.x = -100;
+		}
 		enemyOne.x += (2 * dt);
 		enemyTwo.x += (3 * dt);
 		enemyThree.x += (5 * dt);
