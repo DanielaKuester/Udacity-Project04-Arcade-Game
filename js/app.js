3dd4d47472026a68fd9843f8cfce25e699ce5f5b
Daniela Küster <daniela.kuester@gmx.net>
2018-06-26 15:07:16 +0200

    Add movement to bugs/enemies

--- js/app.js
+++ js/app.js
@@ -17,6 +17,12 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+		enemyOne.x += 0.4;
+		enemyTwo.x += 0.2;
+		enemyThree.x += 0.3;
+		enemyFour.x += 0.2;
+		enemyFive.x += 0.3;
+		enemySix.x += 0.1;
 };
 
 // Draw the enemy on the screen, required method for game
