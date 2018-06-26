e5138dff70feef8f6cff7e0a2520360656ad93ed
Daniela Küster <daniela.kuester@gmx.net>
2018-06-26 15:22:54 +0200

    Make game speed similar on all computers

--- js/app.js
+++ js/app.js
@@ -17,12 +17,12 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-		enemyOne.x += 0.4;
-		enemyTwo.x += 0.2;
-		enemyThree.x += 0.3;
-		enemyFour.x += 0.2;
-		enemyFive.x += 0.3;
-		enemySix.x += 0.1;
+		enemyOne.x += (2 * dt);
+		enemyTwo.x += (3 * dt);
+		enemyThree.x += (5 * dt);
+		enemyFour.x += (4 * dt);
+		enemyFive.x += (3.5 * dt);
+		enemySix.x += (2.5 * dt);
 };
 
 // Draw the enemy on the screen, required method for game
