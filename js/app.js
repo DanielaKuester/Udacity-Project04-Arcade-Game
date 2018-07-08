4617e24dfceaac6a803224792a9d8b828c016171
Daniela Küster <daniela.kuester@gmx.net>
2018-07-08 14:39:31 +0200

    Refactor: Unify whitespace (all tabstops)

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 // TODO: Add reference to study jam by Edoh Kodjo: https://youtu.be/mgFWZGpj3IE?t=1
 // Enemies our player must avoid
 var Enemy = function(x, y, speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+		// Variables applied to each of our instances go here,
+		// we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+		// The image/sprite for our enemies, this uses
+		// a helper we've provided to easily load images
+		this.sprite = 'images/enemy-bug.png';
 		this.x = x;
 		this.y = y;
 		this.speed = speed;
@@ -15,9 +15,9 @@ var Enemy = function(x, y, speed) {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+		// You should multiply any movement by the dt parameter
+		// which will ensure the game runs at the same speed for
+		// all computers.
 		if (this.x >= 505) {
 				this.x = -100;
 		}
@@ -47,7 +47,7 @@ Enemy.prototype.update = function(dt) {
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+		ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now write your own player class
@@ -147,12 +147,12 @@ player.handleInput = function (pressKey) {
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+		var allowedKeys = {
+				37: 'left',
+				38: 'up',
+				39: 'right',
+				40: 'down'
+		};
 
-    player.handleInput(allowedKeys[e.keyCode]);
+		player.handleInput(allowedKeys[e.keyCode]);
 });
